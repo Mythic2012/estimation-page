657b1b57225efda8b4abb4662c7ec56105060d6d
Mythic2012 <mythicspread@gmail.com>
2024-11-24 01:28:47 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function displayInfo(type){
+    window.alert(`Add a popup to display extra info (further characteristics, billing in detail) about ${type} here!`)
+}
+
+function showEstimation(){
+    document.getElementById("estimation").style.display = "block"
+}
+
+function showExtraEstimation(){
+    window.alert("This will display the length, width & height options")
+}
